1d95f90529815670e90a6e01aa2b6ed9c27b5111
Milo Todorovich <milo.todorovich@gmail.com>
2025-01-13 19:32:06 -0600

    Example: defprotocol and defrecord (#5)
    * Protocol + record example
    * Update README.md

---
+++ src/this_project/protocol_record.clj
@@ -0,0 +1,35 @@
+(ns this-project.protocol-record)
+
+(defprotocol Finder
+  (find-product [this id])
+  (find-price [this product-id]))
+
+(defrecord DBFinder [_]
+  Finder
+  (find-product [_ id]
+    (println "This is SLOW....")
+    (Thread/sleep 1000)
+    {:product-id id})
+  (find-price [_ product-id]
+    (println "This is SLOW....")
+    (Thread/sleep 1000)
+    {:product-id product-id :price 100}))
+
+(defrecord MockFinder []
+  Finder
+  (find-product [_ id]
+    (println "This is FAST")
+    {:product-id id})
+  (find-price [_ product-id]
+    (println "This is FAST")
+    {:product-id product-id :price 10}))
+
+(defn controller-method [finder]
+  (let [product (find-product finder 1)
+        price (find-price finder (:product-id product))]
+    (println (str "Found the product: " product))
+    (println (str "The price is: " price))))
+
+(time (controller-method (->MockFinder)))
+(time (controller-method (->DBFinder {})))
+
